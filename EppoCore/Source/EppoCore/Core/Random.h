87af275f9b4e89238ce1de7a36cf2e4525ecdc42
Niels Eppenhof <n.eppenhof@gmail.com>
2024-01-02 16:35:34 +0100

    Added fast random

--- EppoCore/Source/EppoCore/Core/Random.h
+++ EppoCore/Source/EppoCore/Core/Random.h
@@ -6,14 +6,36 @@
 
 namespace Eppo
 {
-	static std::random_device s_RandomDevice;
-	static std::mt19937_64 s_Engine(s_RandomDevice());
-	static std::mt19937 s_Engine32(s_RandomDevice());
-
-	static std::uniform_real_distribution<float> s_UniformRealDistribution;
-
-	namespace Random
+	class FastRandom
 	{
+	public:
+		// PCG Hash: https://www.reedbeta.com/blog/hash-functions-for-gpu-rendering/
+		// Returns an integer between 0 and UINT32_MAX
+		static uint32_t PCG_Hash(uint32_t input)
+		{
+			uint32_t state = input * 747796405u + 2891336453u;
+			uint32_t word = ((state >> ((state >> 28u) + 4u)) ^ state) * 277803737u;
+			return (word >> 22u) ^ word;
+		}
+
+		// Returns a float between 0 and 1
+		static float Float(uint32_t& seed)
+		{
+			seed = PCG_Hash(seed);
+			return (float)seed / (float)UINT32_MAX;
+		}
+
+		static glm::vec3 InUnitSphere(uint32_t& seed)
+		{
+			return glm::normalize(
+				glm::vec3(Float(seed) * 2.0f - 1.0f, Float(seed) * 2.0f - 1.0f, Float(seed) * 2.0f - 1.0f)
+			);
+		}
+	};
+
+	class Random
+	{
+	public:
 		static int32_t Int32(int32_t min = INT32_MIN, int32_t max = INT32_MAX)
 		{
 			std::uniform_int_distribution<int32_t> distribution(min, max);
@@ -23,7 +45,7 @@ namespace Eppo
 		static int64_t Int64(int64_t min = INT64_MIN, int64_t max = INT64_MAX)
 		{
 			std::uniform_int_distribution<int64_t> distribution(min, max);
-			return distribution(s_Engine);
+			return distribution(s_Engine64);
 		}
 
 		static uint32_t UInt32(uint32_t min = 0, uint32_t max = UINT32_MAX)
@@ -35,7 +57,7 @@ namespace Eppo
 		static uint64_t UInt64(uint64_t min = 0, uint64_t max = UINT64_MAX)
 		{
 			std::uniform_int_distribution<uint64_t> distribution(min, max);
-			return distribution(s_Engine);
+			return distribution(s_Engine64);
 		}
 
 		static float Float(float min = FLT_MIN, float max = FLT_MAX)
@@ -61,5 +83,9 @@ namespace Eppo
 			glm::vec4 result = glm::vec4(Float(min, max), Float(min, max), Float(min, max), Float(min, max));
 			return result;
 		}
+
+	private:
+		static std::mt19937_64 s_Engine64;
+		static std::mt19937 s_Engine32;
 	};
 }
