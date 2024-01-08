ac66f730600207f1874c87bc8bb924216c514a2f
Niels Eppenhof <n.eppenhof@gmail.com>
2024-01-08 21:54:56 +0100

    Updated asserts for linux

--- EppoCore/Vendor/stb/stb_image.cpp
+++ EppoCore/Vendor/stb/stb_image.cpp
@@ -1,4 +1,9 @@
 #include "pch.h"
 
+// Todo: This is a workaround and should not stay since it disables simd optimization
+#ifdef EPPO_LINUX
+    #undef __OPTIMIZE__
+#endif
+
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
