46d4d70ccd9c51d770fe2f1d68cdf0cc19d1e32a
Niels Eppenhof <n.eppenhof@gmail.com>
2025-01-08 22:49:05 +0100

    Remove GL buffer and add normal buffer

--- EppoCore/Source/EppoCore/Core/Buffer.h
+++ EppoCore/Source/EppoCore/Core/Buffer.h
@@ -4,22 +4,101 @@
 
 namespace Eppo
 {
-	class Buffer
-	{
-	public:
-		Buffer(uint32_t size, uint32_t binding);
-		~Buffer();
+	struct Buffer
+    {
+        uint8_t* Data = nullptr;
+        uint32_t Size = 0;
 
-		void SetData(void* data, uint32_t size);
+        Buffer() = default;
 
-		uint32_t GetSize() const { return m_Size; }
+        Buffer(const uint32_t size)
+        {
+            Allocate(size);
+        }
 
-		glm::vec4* MapBuffer() const;
-		void UnmapBuffer() const;
+        static Buffer Copy(const Buffer other)
+        {
+            const Buffer result(other.Size);
+            memcpy(result.Data, other.Data, other.Size);
+            return result;
+        }
 
-	private:
-		uint32_t m_RendererID;
-		uint32_t m_Size;
-		uint32_t m_Binding;
-	};
+        static Buffer Copy(const uint8_t* data, const uint32_t size)
+        {
+            const Buffer result(size);
+            memcpy(result.Data, data, size);
+            return result;
+        }
+
+        static Buffer Copy(const void* data, const uint32_t size)
+        {
+            const Buffer result(size);
+            memcpy(result.Data, data, size);
+            return result;
+        }
+
+        void Allocate(const uint32_t size)
+        {
+            Release();
+
+            Data = new uint8_t[size];
+            Size = size;
+        }
+
+        void Release()
+        {
+            delete[] Data;
+            Data = nullptr;
+            Size = 0;
+        }
+
+        template<typename T>
+        T* As()
+        {
+            return (T*)Data;
+        }
+
+        explicit operator bool() const
+        {
+            return static_cast<bool>(Data);
+        }
+    };
+
+	class ScopedBuffer
+    {
+    public:
+        ScopedBuffer() = default;
+
+        explicit ScopedBuffer(const Buffer buffer)
+            : m_Buffer(buffer)
+        {}
+
+        ScopedBuffer(const uint32_t size)
+            : m_Buffer(size)
+        {}
+
+        ~ScopedBuffer()
+        {
+            m_Buffer.Release();
+        }
+
+        [[nodiscard]] uint8_t* Data() const
+        {
+            return m_Buffer.Data;
+        }
+
+        [[nodiscard]] uint32_t Size() const
+        {
+            return m_Buffer.Size;
+        }
+
+        template<typename T>
+        T* As()
+        {
+            return m_Buffer.As<T>();
+        }
+
+    private:
+        Buffer m_Buffer;
+    };
 }
