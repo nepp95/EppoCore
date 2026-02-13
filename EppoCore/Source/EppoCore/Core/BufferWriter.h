6d6c6c4ba18a72a80a8bd4bdfafc4d29ae7be9d2
Niels Eppenhof <n.eppenhof@gmail.com>
2026-02-13 18:39:57 +0100

    Fixed more errors.

--- EppoCore/Source/EppoCore/Core/BufferWriter.h
+++ EppoCore/Source/EppoCore/Core/BufferWriter.h
@@ -1,38 +1,35 @@
 #pragma once
 
-#include "EppoCore/Core/Base.h"
 #include "EppoCore/Core/Buffer.h"
 
 namespace Eppo
 {
-	class BufferWriter
-	{
-	public:
-		BufferWriter(Buffer buffer, uint32_t offset = 0)
-			: m_Buffer(buffer), m_Offset(offset)
-		{}
+    class BufferWriter
+    {
+    public:
+        BufferWriter(Buffer buffer, const uint64_t offset = 0)
+            : m_Buffer(buffer), m_Offset(offset)
+        {
+        }
 
-		void Write(Buffer srcBuffer)
-		{
-			EP_ASSERT(m_Buffer.Size >= srcBuffer.Size + m_Offset);
-			memcpy(m_Buffer.Data, srcBuffer.Data, srcBuffer.Size);
-			m_Offset += srcBuffer.Size;
-		}
-		
-		void Write(void* data, uint32_t dataSize)
-		{
-			EP_ASSERT(m_Buffer.Size >= m_Offset + dataSize);
-			memcpy(m_Buffer.Data, data, dataSize);
-			m_Offset += dataSize;
-		}
+        void Write(Buffer srcBuffer)
+        {
+            EP_ASSERT(m_Buffer.Size >= srcBuffer.Size + m_Offset);
+            memcpy(m_Buffer.Data, srcBuffer.Data, srcBuffer.Size);
+            m_Offset += srcBuffer.Size;
+        }
 
-		uint32_t BytesWritten() const
-		{
-			return m_Offset;
-		}
+        void Write(void* data, const uint64_t dataSize)
+        {
+            EP_ASSERT(m_Buffer.Size >= m_Offset + dataSize);
+            memcpy(m_Buffer.Data, data, dataSize);
+            m_Offset += dataSize;
+        }
 
-	private:
-		Buffer m_Buffer;
-		uint32_t m_Offset;
-	};
+        uint64_t BytesWritten() const { return m_Offset; }
+
+    private:
+        Buffer m_Buffer;
+        uint64_t m_Offset;
+    };
 }
